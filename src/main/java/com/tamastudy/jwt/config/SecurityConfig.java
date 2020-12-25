4fc21da5b9a353cd9d14f152bb00c99ee00610c6
jonsoku2 <whdtjr2792@gmail.com>
2020-12-25 17:08:27 +0900

    jwt Filter 등록 테스트

--- src/main/java/com/tamastudy/jwt/config/SecurityConfig.java
+++ src/main/java/com/tamastudy/jwt/config/SecurityConfig.java
@@ -1,12 +1,14 @@
 package com.tamastudy.jwt.config;
 
+import com.tamastudy.jwt.filter.MyFilter3;
 import lombok.RequiredArgsConstructor;
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -17,7 +19,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // https://blog.naver.com/getinthere/222094919059
+        // BasicAuthenticationFilter 가 실행되기 전에 MyFilter1 가 걸린다.
+        http.addFilterBefore(new MyFilter3(), BasicAuthenticationFilter.class);
         http.csrf().disable();
+        // SessionCreationPolicy.STATELESS : 세션을 사용하지 않겠다.
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .addFilter(corsFilter) // 모든 요청은 이 filter 를 무조건 타게된다.-> controller @CrossOrigin(인증X), 시큐리티 필터에 등록(인증O)
